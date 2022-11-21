2135de0e92380ac0511aa8de2b56f83cd3421ce4
Katerina Maria Gerakianaki <katerinagerak99@gmail.com>
2022-11-21 16:19:53 +0200

    This is the table page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PhoneComponent } from './pages/phone/phone.component';
+import { TableComponent } from './pages/table/table.component';
 import { ItemShopComponent } from './pages/item-shop/item-shop.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'tasks', loadChildren: () => import('./pages/tasks/tasks.module').then(m => m.TasksModule) },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'phone', loadChildren: () => import('./pages/phone/phone.module').then(m => m.PhoneModule) },
+  { path: 'table',loadChildren: () => import('./pages/table/table.module').then(m=>m.TableModule)},
   { path: 'item-shop', component: ItemShopComponent},
   //{ path: '**', redirectTo: 'home', pathMatch: 'full' },
   //{ path: '**', redirectTo: 'phone', pathMatch: 'full' },
