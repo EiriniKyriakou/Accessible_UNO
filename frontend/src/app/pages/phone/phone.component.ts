0572d15d7bc29aed3d6e6c5fcb7e4010701620b3
Eirini Kyriakou <73352769+EiriniKyriakou@users.noreply.github.com>
2022-11-21 16:09:43 +0200

    phone component

---
+++ frontend/src/app/pages/phone/phone.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './phone.component.html',
+  styleUrls: ['./phone.component.scss']
+})
+export class PhoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
