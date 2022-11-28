542ec83e69e9ebd113694550b7590f3820ffb6f7
Katerina Maria Gerakianaki <katerinagerak99@gmail.com>
2022-11-28 21:16:32 +0200

    some more front on sidebar

--- frontend/src/app/pages/sidebar/sidebar.component.ts
+++ frontend/src/app/pages/sidebar/sidebar.component.ts
@@ -8,11 +8,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class SidebarComponent  {
   isHided=false 
   inProfile=false
-
+  editUsername=false
+  editPassword=false
+  editAccessibility=false
   @Output() onClickAction: EventEmitter <any> = new EventEmitter;
 
   hideOnAction(){
     this.isHided = !this.isHided;
+    this.editUsername=false
+    this.editPassword=false
+    this.editAccessibility=false
     this.onClickAction.emit(this.isHided);
     if(this.inProfile){
       this.inProfile=!this.inProfile
@@ -20,6 +25,27 @@ export class SidebarComponent  {
     
   }
   profile(){
+    this.inProfile=true
+  }
+  username(){
+    this.editUsername=true
+    this.inProfile=false
+  }
+  editUsernameFunction(){
+    this.inProfile=true
+  }
+  password(){
+    this.editPassword=true
+    this.inProfile=false
+  }
+  editPasswordFunction(){
+    this.inProfile=true
+  }
+  accessibility(){
+    this.editAccessibility=true
+    this.inProfile=false
+  }
+  editAccessibilityFunction(){
     this.inProfile=true
   }
 }
