92f1691ecbb1fdaecc0a3a9d81317bf35eef219e
Katerina Maria Gerakianaki <katerinagerak99@gmail.com>
2022-11-28 12:30:16 +0200

    Scores on tv

--- frontend/src/app/pages/tv/tv.component.ts
+++ frontend/src/app/pages/tv/tv.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TVComponent implements OnInit {
 
-  constructor() { }
 
   ngOnInit() { }
 
+  constructor( private renderer: Renderer2 ) {
+    this.renderer.setStyle(document.body, 'background-image', 'url(../../../assets/backgrounds/background-tv-wall.png)');
+  }
 }
--- frontend/src/app/pages/wall/wall.component.ts
+++ frontend/src/app/pages/wall/wall.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WallComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( private renderer: Renderer2 ) {
+    this.renderer.setStyle(document.body, 'background-image', 'url(../../../assets/backgrounds/background-tv-wall.png)');
+  }
   ngOnInit() { }
 
 }
