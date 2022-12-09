105e9c5b8719136c2717e1b6f804dff46955c332
Eirini Kyriakou <73352769+EiriniKyriakou@users.noreply.github.com>
2022-12-09 13:19:10 +0200

    localhost

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //host: 'http://localhost:8080', // change "localhost" to your IP address
-  host: 'http://147.52.146.213:8080', // change "localhost" to your IP address
+  host: 'http://localhost:8080', // change "localhost" to your IP address
+  //host: 'http://147.52.146.213:8080', // change "localhost" to your IP address
 };
 /*
  * For easier debugging in development mode, you can import the following file
