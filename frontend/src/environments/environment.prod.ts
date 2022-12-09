fed0a53a6975eac9c51b4c47b9a338eaceaaf968
Eirini Kyriakou <73352769+EiriniKyriakou@users.noreply.github.com>
2022-12-09 10:56:28 +0200

    phonegame for phone

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  host: 'http://localhost:8080', // change "localhost" to your IP address
-  //host: 'http://192.168.1.3:8080',
+  //host: 'http://localhost:8080', // change "localhost" to your IP address
+  host: 'http://147.52.146.213:8080',
 };
