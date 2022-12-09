617c6c8a1311549d47c98eb8c8dc9a07e30374ed
Katerina Maria Gerakianaki <77936483+categerjsy@users.noreply.github.com>
2022-12-09 18:29:51 +0200

    more style for when i can code

--- frontend/src/app/pages/card/card.component.ts
+++ frontend/src/app/pages/card/card.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { CardModel } from 'src/app/global/models/cards/card.model';
 
+import { CardModel } from 'src/app/global/models/cards/card.model';
+import { Component, OnInit, Renderer2} from '@angular/core';
+import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',
@@ -12,5 +13,13 @@ export class CardComponent  {
     name: "Red.png",
     number:"5"
   });
+
+
+  // constructor(
+  //   private socketService: SocketsService,
+  //   private renderer: Renderer2
+  // ) {
+  //   this.renderer.setStyle(document.div, 'background-image', 'url(../../../assets/backgrounds/background.png)');
+  // }
   
 }
