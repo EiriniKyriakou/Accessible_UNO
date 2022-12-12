465a77c9349788e7247f71a5eadad92bfd894972
Katerina Maria Gerakianaki <77936483+categerjsy@users.noreply.github.com>
2022-12-12 16:19:42 +0200

    Color css ngclass

--- frontend/src/app/pages/card/card.component.ts
+++ frontend/src/app/pages/card/card.component.ts
@@ -1,48 +1,72 @@
-import {CardModel} from 'src/app/global/models/cards/card.model';
-import {Component, OnInit, Renderer2, Input} from '@angular/core';
-import {SocketsService} from 'src/app/global/services/sockets/sockets.service';
-import {GameModel} from 'src/app/global/models/games/game.model';
-import {GamesService} from 'src/app/global/services/games/game.service';
-@Component({selector: 'card', templateUrl: './card.component.html', styleUrls: ['./card.component.css']})
+import { CardModel } from 'src/app/global/models/cards/card.model';
+import { Component, OnInit, Renderer2, Input } from '@angular/core';
+import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
+import { GameModel } from 'src/app/global/models/games/game.model';
+import { GamesService } from 'src/app/global/services/games/game.service';
+@Component({
+  selector: 'card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
 export class CardComponent {
-    // protected selectedCard: CardModel = new CardModel({
-    //     name: "Red.png",
-    //     number:"5"
-    // });
-    public game = new GameModel();
+  public game = new GameModel();
 
-    public newDeck : string[] = [];
+  public newDeck: string[] = [];
 
-    @Input()selectedCard : CardModel | undefined;
-    test : any;
-    constructor(private gamesService : GamesService) {
-        setTimeout(() => {
-            if (this.selectedCard != undefined) {
-                this.test = new CardModel({
-                    name: this.selectedCard ?. name,
-                    number: this.selectedCard ?. number
-                });
+  @Input() selectedCard: CardModel | undefined;
+  test: any;
+  constructor(private gamesService: GamesService) {
+    setTimeout(() => {
+      if (this.selectedCard != undefined) {
+        this.test = new CardModel({
+          name: this.selectedCard?.name,
+          number: this.selectedCard?.number,
+        });
 
-                this.gamesService.getActive(true).subscribe((result : any) => {
-                    var current_game = result[0];
+        this.gamesService.getActive(true).subscribe((result: any) => {
+          var current_game = result[0];
 
-                    if (JSON.stringify(current_game) === '[]') {
-                        console.log('empty');
-                    } else {
-                        this.game = current_game;
-                        var firstCard = current_game.cards_on_deck[0];
-                        console.log(firstCard);
-                        current_game.played_cards.push(firstCard);
-                        console.log(current_game.played_cards);
-                        current_game.cards_on_deck.shift();
-                        console.log(current_game.cards_on_deck);
-                        let newGame = this.game;
-                        this.gamesService.update(newGame).subscribe((result : any) => {});
-                    }
-                });
-
-
-            }
-        }, 1000);
+          if (JSON.stringify(current_game) === '[]') {
+            console.log('empty');
+          } else {
+            this.game = current_game;
+            var firstCard = current_game.cards_on_deck[0];
+            current_game.played_cards.push(firstCard);
+            current_game.cards_on_deck.shift();
+            let newGame = this.game;
+            this.gamesService.update(newGame).subscribe((result: any) => {});
+          }
+        });
+      }
+    }, 1000);
+  }
+  ngOnInit() { }
+ 
+  checkColor() {
+    var ret = '';
+    switch (this.selectedCard?.name) {
+      case 'Red.png': {
+        ret = 'letterRed';
+        break;
+      }
+      case 'Green.png': {
+        ret = 'letterGreen';
+        break;
+      }
+      case 'Blue.png': {
+        ret = 'letterBlue';
+        break;
+      }
+      case 'Yellow.png': {
+        ret = 'letterYellow';
+        break;
+      }
+      default: {
+        //statements;
+        break;
+      }
     }
+
+    return ret;
+  }
 }
