581721c6bb8a66cec6593ac53fbdfc52d3a8eaa6
Katerina Maria Gerakianaki <77936483+categerjsy@users.noreply.github.com>
2022-12-22 11:31:47 +0200

    wave

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  host: 'http://localhost:8080', // change "localhost" to your IP address
-  //host: 'http://147.52.211.175:8080',
+  // host: 'http://localhost:8080', // change "localhost" to your IP address
+  host: 'http://139.91.80.120:8080',
 };
