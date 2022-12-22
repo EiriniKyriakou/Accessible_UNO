a3084f78b9dfb053836c53bc5fd1b0186bf40df6
Katerina Maria Gerakianaki <77936483+categerjsy@users.noreply.github.com>
2022-12-22 12:34:28 +0200

    Colors in  waves

--- frontend/src/app/pages/wall/wall.component.ts
+++ frontend/src/app/pages/wall/wall.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { CardModel } from 'src/app/global/models/cards/card.model';
+import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,13 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 })
 export class WallComponent implements OnInit {
   condition = false
-  constructor(private renderer: Renderer2) {
+  start = true
+  red = false
+  blue = false
+  yellow = false
+  green = false
+  constructor(private renderer: Renderer2,
+    private socketService: SocketsService) {
     this.renderer.setStyle(document.body, 'background-image', 'url(../../../assets/backgrounds/background-tv-wall.png)');
 
     // setTimeout(()=>{ 
@@ -16,7 +24,40 @@ export class WallComponent implements OnInit {
     //                }
     // ,10000);
 
+    this.socketService.subscribe('card_table', (card: CardModel) => {
+      this.start = false
+      this.red = false
+      this.blue = false
+      this.yellow = false
+      this.green = false
+      switch (card.color) {
+        case 'Red.png': {
+          this.red=true;
+          break;
+        }
+        case 'Green.png': {
+          this.green = true;
+          break;
+        }
+        case 'Blue.png': {
+          this.blue= true;
+          break;
+        }
+        case 'Yellow.png': {
+          this.yellow= true;
+          break;
+        }
+        default: {
+          //statements;
+          break;
+        }
+      }
+    });
+
   }
+
   ngOnInit() { }
+
+
 
 }
