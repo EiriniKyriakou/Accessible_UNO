3680932cfc61cd2440c6c2b4a2987631c87de12e
Eirini Kyriakou <73352769+EiriniKyriakou@users.noreply.github.com>
2023-01-13 17:38:11 +0200

    Player can't join 2 times

--- frontend/src/app/pages/table/tablewaiting/tablewaiting.component.ts
+++ frontend/src/app/pages/table/tablewaiting/tablewaiting.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Renderer2, Output, ElementRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
 import { GamesService } from 'src/app/global/services/games/game.service';
 import { PlayerModel } from 'src/app/global/models/players/player.model';
@@ -11,12 +11,12 @@ import { PlayersService } from 'src/app/global/services/players/players.service'
   templateUrl: './tablewaiting.component.html',
   styleUrls: ['./tablewaiting.component.scss']
 })
-export class TableWaitingComponent implements OnInit { 
+export class TableWaitingComponent implements OnInit {
   public game = new GameModel();
   //public players: string[] = [];
 
   constructor(
-    private router: Router, 
+    private router: Router,
     private socketService: SocketsService,
     private renderer: Renderer2,
     private gamesService: GamesService,
@@ -27,42 +27,43 @@ export class TableWaitingComponent implements OnInit {
   ngOnInit() {
     setTimeout(() => this.changePage(), 60000);  //60s
     setTimeout(() => {
-      this.gamesService.getActive(true).subscribe((result:any) => {
-        if(JSON.stringify(result) === "[]"){
+      this.gamesService.getActive(true).subscribe((result: any) => {
+        if (JSON.stringify(result) === "[]") {
           console.log("No active game")
-        }else{
+        } else {
           this.game = result[0];
         }
       });
 
       this.socketService.subscribe("player_joined", (data: any) => {
-        
-        this.game.players.push(data._id);
-        console.log("Joined player: " + data._id)
-        if(data.dysrhythmia===true){
-          this.game.dysrhythmia = true;
-        }
-        if (data.dyslexia === true){
-          this.game.dyslexia = true;
-        }
-        if (data.impairedVision === true){
-          this.game.impairedVision = true;
-        }
-        if (data.colorblindness == true){
-          this.game.colorblindness = true;
+        if (!this.game.players.includes(data._id)) {
+          this.game.players.push(data._id);
+          console.log("Joined player: " + data.username)
+          if (data.dysrhythmia === true) {
+            this.game.dysrhythmia = true;
+          }
+          if (data.dyslexia === true) {
+            this.game.dyslexia = true;
+          }
+          if (data.impairedVision === true) {
+            this.game.impairedVision = true;
+          }
+          if (data.colorblindness == true) {
+            this.game.colorblindness = true;
+          }
         }
       });
-    },1000);
-    
+    }, 1000);
+
   }
 
-  changePage(){
+  changePage() {
     //let newGame=this.game;
     console.log(this.game)
     //newGame.players=this.players;
-    this.gamesService.update(this.game).subscribe((result:any) => {
+    this.gamesService.update(this.game).subscribe((result: any) => {
     });
     this.socketService.publish("game_start", this.game);
-    setTimeout(() => {this.router.navigate(['/tablegame']);},1000);
+    setTimeout(() => { this.router.navigate(['/tablegame']); }, 1000);
   }
 }
